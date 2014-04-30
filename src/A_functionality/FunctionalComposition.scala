bfe1d7a6e1830f6f168717e1d32b9089191c4e64
Simo Martomaa <smartoma@gmail.com>
2014-04-30 16:36:27 +0200

    fixed test

--- src/A_functionality/FunctionalComposition.scala
+++ src/A_functionality/FunctionalComposition.scala
@@ -2,7 +2,7 @@ package A_functionality
 
 object FunctionalComposition {
 
-  def greet(person: String) = "Hello " + person + "!"
+  def greet(person: String) = "Hello, " + person + "!"
   def printer(text: String) = { println(text); text }
 
   // Combine the two above functions to a single function
